429645688683577436726f0d9b2ee1b4e4bffd21
JuYoung choi <>
2024-04-20 09:24:59 +0900

    chore: Project 생성.

---
+++ jyChoiReportUITests/jyChoiReportUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  jyChoiReportUITestsLaunchTests.swift
+//  jyChoiReportUITests
+//
+//  Created by JuYoung choi on 4/20/24.
+//
+
+import XCTest
+
+final class jyChoiReportUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
